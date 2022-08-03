ab777d280f1d35eed27df0797f05084bd7303ff6
Mishanana20 <mishanana20@gmail.com>
2022-08-04 00:12:33 +0700

    Поиск производится по Имени Актера, Жанру и НАзванию Фильма

--- HelloAppEmpty/Program.cs
+++ HelloAppEmpty/Program.cs
@@ -19,11 +19,12 @@ app.Run(async (context) =>
    // movies = SelectMovies.FindMovies("", "", "");
     if (path == "/api/users" && request.Method == "GET")
     {
-        //await GetAllPeople(response);
+        await GetAllPeople(response);
     }
     else if (path == "/api/users" && request.Method == "POST")
     {
         await FindFilms(response, request);
+
     }
     else
     {
@@ -41,24 +42,52 @@ async Task GetAllPeople(HttpResponse response)
     //нужна функция их получения
     foreach (var movie in SelectMovies.FindMovies("", "", ""))
     {
-        movies.Add(new Film { Id = movie.Id, Name = movie.Name, Description = movie.Description });
+        movies.Add(new Film { 
+            Id = movie.Id, 
+            Name = movie.Name, 
+            Description = movie.Description,
+            Actors = AcotrsInMovie(movie), 
+            Genres = GenresInMovie(movie)
+        });
     }
     await response.WriteAsJsonAsync(movies);
 }
 
+string AcotrsInMovie(Movie movie)
+{
+    List<string> Actor = new List<string>{ };
+    foreach (var actor in movie.Actors)
+    {
+      Actor.Add($"{actor.FirstName}" +" "+ $"{actor.SecondName}");
+    }
+
+    return String.Join(", ", Actor);
+}
+string GenresInMovie(Movie movie)
+{
+    List<string> Genre = new List<string> { };
+    foreach (var genre in movie.Genres)
+    {
+        Genre.Add($"{genre.Name}");
+    }
+    return String.Join(", ", Genre);
+}
 async Task FindFilms(HttpResponse response, HttpRequest request)
 {
     try
     {
         movies.Clear();
-        //movies = null!;
         // получаем данные пользователя
         FindFilm parametersFindFilms = await request.ReadFromJsonAsync<FindFilm>();
-        //Console.WriteLine(parametersFindFilms.Name + "'NJ ;T ABKMV \n !");
-       
         foreach (var movie in SelectMovies.FindMovies(parametersFindFilms.Name, parametersFindFilms.Actor, parametersFindFilms.Genre))
         {
-            movies.Add(new Film { Id = movie.Id, Name = movie.Name, Description = movie.Description });
+            movies.Add(new Film { 
+                Id = movie.Id, 
+                Name = movie.Name, 
+                Description = movie.Description,
+                Actors = AcotrsInMovie(movie),
+                Genres = GenresInMovie(movie)
+            });
         }
         await response.WriteAsJsonAsync(movies);
     }
@@ -74,6 +103,8 @@ public class Film
     public int Id { get; set; } = 0;
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
+    public string Actors { get; set; } = "";
+    public string Genres { get; set; } = "";
 }
 
 public class FindFilm
